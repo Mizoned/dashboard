4cf81c493c2929504526e4b3556eba14e58aeeee
Valery Shcherbinin <fundroin@mail.ru>
2022-10-04 18:06:50 +0500

    подключает шрифты и стили в проект

--- src/main.js
+++ src/main.js
@@ -3,4 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import '@/assets/styles/main.scss';
+import 'normalize.css';
+
+const app = createApp(App);
+
+app
+    .use(store)
+    .use(router)
+    .mount('#app');
