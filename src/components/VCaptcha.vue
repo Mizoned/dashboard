368b67bcd8326cf628d07131960bdbb433ff473e
Valery Shcherbinin <fundroin@mail.ru>
2022-10-24 18:39:47 +0500

    Добавляет компонент капчи

---
+++ src/components/VCaptcha.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="v-captcha">This site is protected by reCAPTCHA and the Google Privacy Policy.</div>
+</template>
+
+<script>
+export default {
+  name: "VCaptcha"
+}
+</script>
+
+<style scoped lang="scss">
+  .v-captcha {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 24px;
+    letter-spacing: -0.01em;
+    color: var(--neutral-shade-dark-04-40-color);
+  }
+</style>
