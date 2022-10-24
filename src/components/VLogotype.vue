5b9be0c81c3bef8b2a1a4e76050102a992a4a6e1
Valery Shcherbinin <fundroin@mail.ru>
2022-10-24 18:40:09 +0500

    Добавляет компонент логотип

---
+++ src/components/VLogotype.vue
@@ -0,0 +1,17 @@
+<template>
+  <img class="v-logotype" src="@/assets/images/logo.png" alt="">
+</template>
+
+<script>
+export default {
+  name: "VLogotype"
+}
+</script>
+
+<style scoped>
+  .v-logotype {
+    display: block;
+    width: 48px;
+    height: 48px;
+  }
+</style>
