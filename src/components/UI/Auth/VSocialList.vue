bf839da192a630c840b812a795a66952b2c9cbbd
Valery Shcherbinin <fundroin@mail.ru>
2022-10-24 18:36:12 +0500

    Добавляет компонент списка компонентов авторизации

---
+++ src/components/UI/Auth/VSocialList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="socials">
+    <v-google class="socials__social"></v-google>
+    <v-apple class="socials__social"></v-apple>
+  </div>
+</template>
+
+<script>
+import VGoogle from "@/components/UI/Auth/VGoogle";
+import VApple from "@/components/UI/Auth/VApple";
+
+export default {
+  name: "VSocialList",
+  components: { VGoogle, VApple },
+}
+</script>
+
+<style scoped lang="scss">
+  .socials {
+    display: flex;
+    align-self: flex-start;
+    column-gap: 8px;
+  }
+</style>
