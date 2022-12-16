2d3c452be2e251038f3af7f4548f5f87cded7038
Valery Shcherbinin <fundroin@mail.ru>
2022-12-16 13:10:39 +0500

    Правит верстку и добавляет валидацию для поля ввода email

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -8,31 +8,44 @@
     </div>
     <div class="authorization__main">
       <div class="authorization__header">
-        <div class="authorization__main-sign-in">
-          Already a member? <a class="v-link" href="#">Sign in</a>
-        </div>
+        <div class="authorization__header-logotype"><v-logotype class="authorization__logotype"></v-logotype></div>
+        <div class="authorization__main-sign-in">Already a member? <a class="v-link" href="#">Sign in</a></div>
       </div>
       <div class="authorization__container">
         <div class="sign-up">
           <h1 class="sign-up__title">Sign up</h1>
-          <div class="sign-up__container">
+          <div v-if="step === 1" class="sign-up__container">
             <div class="sign-up__socials">
               <div class="sign-up__socials-text">Sign up with Open account</div>
               <v-social-list class="sign-up__list"></v-social-list>
+              <div class="sign-up__hr"></div>
             </div>
-            <div class="sign-up__hr"></div>
-            <form class="sign-up__form form-component">
-              <v-label>
-                <template v-slot:label>Or continue with email address</template>
-                <template v-slot:input>
-                  <v-input-icon type="text" placeholder="Your email" :hasError="form.email.hasError">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M5 6H19C19.3862 6 19.7213 6.21897 19.8879 6.53954L12.5547 11.4283C12.2188 11.6523 11.7812 11.6523 11.4453 11.4283L4.11209 6.53953C4.27868 6.21896 4.61377 6 5 6ZM4 8.8685V17C4 17.5523 4.44772 18 5 18H19C19.5523 18 20 17.5523 20 17V8.86852L13.6641 13.0924C12.6564 13.7642 11.3436 13.7642 10.3359 13.0924L4 8.8685ZM2 7C2 5.34315 3.34315 4 5 4H19C20.6569 4 22 5.34315 22 7V17C22 18.6569 20.6569 20 19 20H5C3.34315 20 2 18.6569 2 17V7Z" fill="#6F767E"/></svg>
-                  </v-input-icon>
-                </template>
-              </v-label>
-              <v-button>Continue</v-button>
+            <div class="sign-up__form form-component">
+              <v-input-icon
+                  type="text"
+                  placeholder="Your email"
+                  v-model="email"
+                  :label-text="'Or continue with email address'"
+                  :isError="v$.email.$invalid && v$.email.$error"
+                  @clear="resetValidate"
+                  @update:modelValue="updateEmail"
+                  @blur="this.v$.email.$touch()"
+              >
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M5 6H19C19.3862 6 19.7213 6.21897 19.8879 6.53954L12.5547 11.4283C12.2188 11.6523 11.7812 11.6523 11.4453 11.4283L4.11209 6.53953C4.27868 6.21896 4.61377 6 5 6ZM4 8.8685V17C4 17.5523 4.44772 18 5 18H19C19.5523 18 20 17.5523 20 17V8.86852L13.6641 13.0924C12.6564 13.7642 11.3436 13.7642 10.3359 13.0924L4 8.8685ZM2 7C2 5.34315 3.34315 4 5 4H19C20.6569 4 22 5.34315 22 7V17C22 18.6569 20.6569 20 19 20H5C3.34315 20 2 18.6569 2 17V7Z" fill="#6F767E"/></svg>
+              </v-input-icon>
+              <v-button :disabled="v$.email.$invalid && v$.email.$error" @click="onSubmit">Continue</v-button>
               <v-captcha></v-captcha>
-            </form>
+            </div>
+          </div>
+          <div v-else class="sign-up__container">
+            <div class="sign-up__socials">
+              <div  class="sign-up__socials-text">We just send you a verify code. Check your inbox to get them.</div>
+            </div>
+            <div class="sign-up__form form-component">
+              <v-code v-model:code="code" v-model:isError="errorCode"></v-code>
+              <v-button @click="errorCode = true">Continue</v-button>
+              <v-captcha></v-captcha>
+            </div>
           </div>
         </div>
       </div>
@@ -48,19 +61,50 @@ import VButton from "@/components/UI/VButton";
 import VCaptcha from "@/components/VCaptcha";
 import VLogotype from "@/components/VLogotype";
 import VPlanIncludes from "@/components/UI/Auth/VPlanIncludes";
+import { useVuelidate } from '@vuelidate/core';
+import { required, email } from '@vuelidate/validators';
+import VCode from "@/components/UI/Auth/VCode.vue";
 
 export default {
   name: "SignUp",
   components: {
+    VCode,
     VInputIcon, VSocialList, VLabel, VButton, VCaptcha, VLogotype, VPlanIncludes
   },
   data() {
     return {
-      form: {
-        email: {
-          hasError: true
-        }
+      v$: useVuelidate(),
+      isAllowSending: false,
+      email: '',
+      step: 1,
+      code: '',
+      errorCode: false
+    }
+  },
+  validations () {
+    return {
+      email: { required, email }
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (!this.v$.email.$invalid && !this.v$.email.$error) {
+        this.step = 2;
+        alert(0);
       }
+    },
+    updateEmail(value) {
+      if (!value) {
+        this.resetValidate();
+      }
+    },
+    resetValidate() {
+      this.email = '';
+      this.v$.email.$reset();
+    },
+    updateCode(value) {
+      console.log(value)
+      this.code = value;
     }
   }
 }
@@ -76,6 +120,10 @@ export default {
     display: grid;
     grid-template-rows: auto 1fr;
     height: 100%;
+
+    @media screen and (max-width: 768px) {
+      display: none;
+    }
   }
 
   &__main {
@@ -92,10 +140,22 @@ export default {
   }
 
   &__header {
+    display: flex;
+    justify-content: space-between;
     padding: 24px;
+
+    &-logotype {
+      display: none;
+
+      @media screen and (max-width: 768px) {
+        display: block;
+      }
+    }
+
   }
 
   &__main-sign-in {
+    margin-left: auto;
     font-style: normal;
     font-weight: 600;
     font-size: 13px;
@@ -104,6 +164,10 @@ export default {
     color: var(--neutral-shade-04-75-color);
     padding: 16px;
     text-align: right;
+  }
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
   }
 }
 
@@ -148,6 +212,7 @@ export default {
     height: 2px;
     background-color: var(--neutral-06-color);
     border-radius: 2px;
+    margin-top: 12px;
   }
 
   &__form {
