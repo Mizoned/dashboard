6bfedce8929e561f6b3274046bc5b3ce8aa4bd67
Valery Shcherbinin <fundroin@mail.ru>
2022-12-19 20:31:06 +0500

    Добавляет подключение директив в проект

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import directives from "@/directives";
 
 import '@/assets/styles/main.scss';
 import 'normalize.css';
 
 const app = createApp(App);
+
+directives.forEach(directive => {
+    app.directive(directive.name, directive);
+});
 
 app
     .use(store)
