6c07837988d1eeaded8c8ed67be1ca22a8033d0a
Valery Shcherbinin <fundroin@mail.ru>
2023-02-02 14:33:00 +0500

    Добавляет компоненты елемент и блок профиля для шапки сайта

---
+++ src/components/UI/Header/ProfileItem.vue
@@ -0,0 +1,73 @@
+<template>
+  <div :class="[ 'profile-item', `profile-item--${ color }` ]">
+    <slot name="svg"></slot>
+    <span class="profile-item__label">{{ label }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileItem",
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      default: 'white'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .profile-item {
+    display: flex;
+    align-items: center;
+    padding: 12px;
+    gap: 12px;
+    cursor: pointer;
+
+    &__label {
+      font-weight: 600;
+      font-size: 15px;
+      line-height: 24px;
+      letter-spacing: -0.01em;
+      cursor: pointer;
+      transition: all 0.3s;
+    }
+
+    &--white {
+      color: var(--neutral-04-color);
+
+      &:hover {
+        &:deep(svg) {
+          path {
+            fill: var(--neutral-01-color);
+          }
+        }
+
+        .profile-item__label {
+          color: var(--neutral-01-color);
+        }
+      }
+    }
+
+    &--purple {
+      color: var(--primary-purple-color);
+      opacity: 1;
+      transition: all 0.3s;
+
+      &:deep(svg) {
+        path {
+          fill: var(--primary-purple-color);
+        }
+      }
+
+      &:hover {
+        opacity: 0.8;
+      }
+    }
+  }
+</style>
