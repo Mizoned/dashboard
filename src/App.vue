904d72d0a0544844fbbb9d7fd45af1742b4bd74d
Valery Shcherbinin <fundroin@mail.ru>
2023-02-06 12:07:13 +0500

    Добавляет переключение темы через компонент ThemeSwitcher

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
-  <router-view/>
+  <div :class="[ 'app', { 'light-theme' : !isDarkThemeMode } ]">
+    <router-view/>
+  </div>
 </template>
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters({
+      isDarkThemeMode: 'theme/isDarkThemeMode'
+    })
+  }
+}
+</script>
