9a7cbeed719741a1477b673c1dfb47af0281ffa5
Valery Shcherbinin <fundroin@mail.ru>
2023-02-17 10:15:50 +0500

    Добавляет компонент оверлея

---
+++ src/components/VOverlay.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="[ 'v-overlay', { 'active': isActive }]"></div>
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  name: "VOverlay",
+  computed: {
+    ...mapState({
+      isActive: state => state.overlay.isOverlayActive
+    }),
+  },
+  methods: {
+    ...mapMutations({
+      toggleIsOverlayActive: 'overlay/toggleIsOverlayActive'
+    })
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .v-overlay {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: var(--neutral-dark-gray-background-color);
+    opacity: 0;
+    visibility: hidden;
+    //transition: background-color 0.3s, visibility 0.3s, opacity 0.3s;
+    z-index: 5;
+
+    &.active {
+      visibility: visible;
+      opacity: 0.9;
+    }
+  }
+</style>
