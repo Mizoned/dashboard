e488c79fa9fce3bd20602875e7c1ae14afab59d5
Valery Shcherbinin <fundroin@mail.ru>
2023-02-25 18:59:30 +0500

    Регистрирует компонент switch глобально

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,7 +1,9 @@
 import VInput from "@/components/UI/VInput.vue";
 import VButton from "@/components/UI/VButton.vue";
+import VSwitch from "@/components/UI/VSwitch.vue";
 
 export default [
     VInput,
-    VButton
+    VButton,
+    VSwitch
 ]
