f35dd8bc64d03c15f26b4912e38e5fc726621a4e
Valery Shcherbinin <fundroin@mail.ru>
2023-02-26 17:36:43 +0500

    Добавляет компонент devider в глобальное подключение компонентов

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import VWidgetTitle from "@/components/VWidgetTitle.vue";
 import VBox from "@/components/VBox.vue";
+import VDivider from "@/components/VDivider.vue";
 
 export default [
     VWidgetTitle,
-    VBox
+    VBox,
+    VDivider
 ];
