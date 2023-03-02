70a1335b7bbed1022c1cc90717a47752bc601dc8
Valery Shcherbinin <fundroin@mail.ru>
2023-03-02 21:30:00 +0500

    Добавляет директиву, позволяющую скролить блок до внутреннего элемента

---
+++ src/directives/VScrollTo.js
@@ -0,0 +1,23 @@
+export default {
+    name: 'scroll-to',
+    mounted(el, binding) {
+        const headerHeight = 96;
+
+        let target = document.querySelector(binding.value.targetSelector);
+        if (!target) {
+            throw new Error(`Внутренний элемент ${binding.value.parentSelector} родителя не определен`)
+        }
+
+        let parent = binding.value.parentSelector ? el.closest(binding.value.parentSelector) : el.parentElement;
+        if (!parent) {
+            throw new Error(`Родительский элемент с селектором ${binding.value.parentSelector} не определен`)
+        }
+
+        el?.addEventListener('click', (e) => {
+            if (target) {
+                let scrollTop = target.getBoundingClientRect().top - headerHeight;
+                parent.scrollTop = scrollTop + parent.scrollTop;
+            }
+        });
+    }
+}
--- src/directives/index.js
+++ src/directives/index.js
@@ -1,7 +1,9 @@
 import VFocus from "@/directives/VFocus";
 import VClickOutside from "@/directives/VClickOutside";
+import VScrollTo from "@/directives/VScrollTo";
 
 export default [
     VFocus,
-    VClickOutside
+    VClickOutside,
+    VScrollTo
 ]
