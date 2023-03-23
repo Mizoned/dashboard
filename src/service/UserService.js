e0c43480fc8ad1d0ed05438eef77154a6bb6b44e
Valery Shcherbinin <fundroin@mail.ru>
2023-03-23 12:51:46 +0500

    Добавляет маршруты для изменения и удаления картинки в профиле в UserService

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,21 +1,21 @@
 import $api from "@/http";
 
 export default class UserService {
-    static async updateProfileData(email, displayName, location, notifyAboutProductUpdates, notifyAboutMarketNewsletter, notifyAboutComments, notifyAboutPurchases) {
+    static async updateProfileData(data) {
         return $api.put('/user/update-profile-data',
-            {
-                email,
-                displayName,
-                location,
-                notifyAboutProductUpdates,
-                notifyAboutMarketNewsletter,
-                notifyAboutComments,
-                notifyAboutPurchases
-            }
+            data
         );
     }
 
     static async updateProfilePassword(oldPassword, newPassword, confirmNewPassword) {
         return $api.put('/user/update-profile-password', { oldPassword, newPassword, confirmNewPassword });
+    }
+
+    static async updateProfilePicture(picture) {
+        return $api.put('/user/update-profile-picture', picture)
+    }
+
+    static async removeProfilePicture() {
+        return $api.delete('/user/remove-profile-picture')
     }
 }
