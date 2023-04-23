c91ebe4c71b67a34296ec60c5ee61d96affc37a6
Valery Shcherbinin <fundroin@mail.ru>
2023-04-23 14:50:37 +0500

    Добавляет новые UI компоненты VLabelBox, VTextarea, VUploadInput

---
+++ src/components/UI/VLabelBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="v-label-box">
+    <v-label :label="label" :tooltipMessage="tooltipMessage" :is-error="isError"/>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "VLabelBox",
+  props: {
+    label: {
+      type: String,
+      default: 'String'
+    },
+    tooltipMessage: {
+      type: String,
+      default: ''
+    },
+    isError: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .v-label-box {
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+  }
+</style>
