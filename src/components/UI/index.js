52059455f40060eaa22d7e2fe28bad537271a2ce
Valery Shcherbinin <fundroin@mail.ru>
2023-04-23 15:12:54 +0500

    Регистрирует глобально новые UI компоненты

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -3,11 +3,19 @@ import VButton from "@/components/UI/VButton.vue";
 import VSwitch from "@/components/UI/VSwitch.vue";
 import VLinkWithIcon from "@/components/UI/VLinkWithIcon.vue";
 import VButtonLink from "@/components/UI/VButtonLink.vue";
+import VTextarea from "@/components/UI/VTextarea.vue";
+import VToggle from "@/components/UI/VToggle.vue";
+import VLabel from "@/components/UI/VLabel.vue";
+import VLabelBox from "@/components/UI/VLabelBox.vue";
 
 export default [
     VInput,
     VButton,
     VSwitch,
     VLinkWithIcon,
-    VButtonLink
+    VButtonLink,
+    VTextarea,
+    VToggle,
+    VLabel,
+    VLabelBox
 ]
