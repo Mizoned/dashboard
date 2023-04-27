85185a550bcc8020f5f46ccf58d1c99820a097a9
Valery Shcherbinin <fundroin@mail.ru>
2023-04-27 22:55:30 +0500

    Исправляет ошибку в компоненте с отображением аватара пользователя

--- src/components/UI/VAvatar.vue
+++ src/components/UI/VAvatar.vue
@@ -1,67 +1,66 @@
 <template>
-  <div :class="['v-avatar', `v-avatar--${ size }`]">
-    <img :src="path" alt="avatar" @error="loadingErrorHandler">
-  </div>
+	<div :class="['v-avatar', `v-avatar--${size}`]">
+		<img :src="path" alt="avatar" @error="loadingErrorHandler" />
+	</div>
 </template>
 
 <script>
 export default {
-  name: "VAvatar",
-  props: {
-    src: {
-      type: String,
-      default: ''
-    },
-    size: {
-      type: String,
-      default: 'lg'
-    }
-  },
-  data() {
-    return {
-      path: ''
-    }
-  },
-  methods: {
-    loadingErrorHandler() {
-      this.path = require('@/assets/images/no-photo.png');
-    }
-  },
-  watch: {
-    src (newVal) {
-      this.path = newVal;
-    }
-  },
-  created() {
-    this.path = this.src;
-  }
-}
+	name: 'VAvatar',
+	props: {
+		src: {
+			type: String,
+			default: ''
+		},
+		size: {
+			type: String,
+			default: 'lg'
+		}
+	},
+	data() {
+		return {
+			path: ''
+		};
+	},
+	watch: {
+		src(newVal) {
+			this.path = newVal;
+		}
+	},
+	created() {
+		this.path = this.src;
+	},
+	methods: {
+		loadingErrorHandler() {
+			this.path = new URL('@/assets/images/no-photo.png', import.meta.url).href;
+		}
+	}
+};
 </script>
 
 <style scoped lang="scss">
-  .v-avatar {
+.v-avatar {
+	&--lg {
+		width: 96px;
+		height: 96px;
+	}
 
-    &--lg {
-      width: 96px;
-      height: 96px;
-    }
+	&--md {
+		width: 48px;
+		height: 48px;
+	}
 
-    &--md {
-      width: 48px;
-      height: 48px;
-    }
+	&--sm {
+		width: 32px;
+		height: 32px;
+	}
 
-    &--sm {
-        width: 32px;
-        height: 32px;
-    }
-
-    img {
-      display: block;
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-      border-radius: 50%;
-    }
-  }
+	img {
+		display: block;
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		border-radius: 50%;
+	}
+}
 </style>
