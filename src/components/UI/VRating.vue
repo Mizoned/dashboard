38a879679f61525d79131390d503f4f5fc9256e1
Valery Shcherbinin <fundroin@mail.ru>
2023-05-03 17:28:49 +0500

    Добавляет компонент для отображения рейтинга продукта

---
+++ src/components/UI/VRating.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="v-rating">
+		<component :is="iconComponentName" :is-active="isActive"></component>
+		<div v-if="isActive" class="v-rating__value">
+			{{ value }} <span v-if="count" class="v-rating__count">({{ count }})</span>
+		</div>
+		<div v-else class="v-rating__not">No ratings</div>
+	</div>
+</template>
+
+<script>
+import VIconStar from '@/components/icons/VIconStar.vue';
+
+export default {
+	name: 'VRating',
+	components: [VIconStar],
+	props: {
+		value: {
+			type: [String, Number],
+			default: 0
+		},
+		count: {
+			type: [String, Number],
+			default: 0
+		}
+	},
+	data() {
+		return {
+			iconComponentName: 'VIconStar'
+		};
+	},
+	computed: {
+		isActive() {
+			return this.value > 0;
+		}
+	}
+};
+</script>
+
+<style scoped lang="scss">
+.v-rating {
+	display: flex;
+	align-items: center;
+	gap: 8px;
+
+	&__value,
+	&__not {
+		font-style: normal;
+		font-weight: 600;
+		font-size: 14px;
+		line-height: 24px;
+		letter-spacing: -0.01em;
+		color: var(--neutral-champagne-color);
+	}
+
+	&__not,
+	&__count {
+		color: var(--primary-dark-white-color);
+	}
+}
+</style>
