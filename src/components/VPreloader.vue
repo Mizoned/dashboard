af4e2d6f71d0608a1034b83a7b2ef2e9d9b7dda5
Valery Shcherbinin <fundroin@mail.ru>
2023-06-06 23:25:24 +0500

    Добавляет компонент VPreloader

---
+++ src/components/VPreloader.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="v-preloader">Loading <v-icon-preloader /></div>
+</template>
+
+<script>
+export default {
+	name: 'VPreloader'
+};
+</script>
+
+<style scoped lang="scss">
+.preloader {
+	display: flex;
+	justify-content: center;
+	column-gap: 8px;
+	align-items: center;
+	color: var(--neutral-champagne-background-color);
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import VWidgetTitle from '@/components/VWidgetTitle.vue';
 import VBox from '@/components/VBox.vue';
 import VDivider from '@/components/VDivider.vue';
+import VPreloader from '@/components/VPreloader.vue';
 
-export default [VWidgetTitle, VBox, VDivider];
+export default [VWidgetTitle, VBox, VDivider, VPreloader];
