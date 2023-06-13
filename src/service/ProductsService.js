47e576a3b9a41a79fe6913b164723ec463e931d3
Valery Shcherbinin <fundroin@mail.ru>
2023-06-13 20:41:09 +0500

    Добавляет сервис по работе с продуктами магазина

---
+++ src/service/ProductsService.js
@@ -0,0 +1,12 @@
+import $api from '@/http';
+import sanitizeParams from '@/utils/sanitizeParams';
+
+export default class ProductsService {
+	static async getProducts(limit = 5, page = 1, search = '') {
+		let params = sanitizeParams({ limit, page, search });
+
+		return $api.get(`/products`, {
+			params
+		});
+	}
+}
