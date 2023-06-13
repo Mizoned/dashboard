c011243778a1c701ad1e13a4eb055dc3ba59f019
Valery Shcherbinin <fundroin@mail.ru>
2023-06-13 20:36:17 +0500

    Добавляет компонент прелоадера приложения

---
+++ src/components/ThePreloader.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="v-loader">
+		<div class="v-loader__icon">
+			<v-logotype />
+		</div>
+	</div>
+</template>
+
+<script>
+import VLogotype from '@/components/VLogotype.vue';
+
+export default {
+	name: 'ThePreloader',
+	components: { VLogotype }
+};
+</script>
+
+<style scoped lang="scss">
+.v-loader {
+	position: fixed;
+	top: 0;
+	right: 0;
+	left: 0;
+	bottom: 0;
+	padding: 32px;
+	background-color: var(--neutral-light-black-background-color);
+	z-index: 100;
+
+	&__icon {
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translateY(-50%);
+
+		&:deep(img) {
+			animation: rotate-vertical 1.3s ease-in-out infinite;
+		}
+	}
+}
+
+@keyframes rotate-vertical {
+	0% {
+		transform: rotateY(0);
+	}
+	100% {
+		transform: rotateY(360deg);
+	}
+}
+</style>
