95df92662b20bb221a7e7af9695b8a59c167c14a
Valery Shcherbinin <fundroin@mail.ru>
2023-06-13 20:57:33 +0500

    Добавляет прелоадер и проверку аутентификации пользователя в системе при входе в приложение

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-	<div :class="['app', { 'light-theme': !isDarkThemeMode }]">
+	<the-preloader v-if="isLoading" />
+	<div v-else :class="['app', { 'light-theme': !isDarkThemeMode }]">
 		<router-view />
 	</div>
 </template>
 <script>
-import { mapGetters } from 'vuex';
+import { mapActions, mapGetters, mapMutations } from 'vuex';
+import ThePreloader from '@/components/ThePreloader.vue';
 
 export default {
+	components: { ThePreloader },
 	computed: {
 		...mapGetters({
-			isDarkThemeMode: 'themeModule/isDarkThemeMode'
+			isDarkThemeMode: 'themeModule/isDarkThemeMode',
+			isLoading: 'authModule/isLoading'
+		})
+	},
+	mounted() {
+		let accessToken = localStorage.getItem('token');
+
+		if (accessToken) {
+			this.setLoading(true);
+
+			this.checkAuthorization()
+				.catch(async () => {
+					this.deleteToken();
+					this.deleteUser();
+					await this.$router.push('/sign-in');
+				})
+				//TODO позже убрать искусственную задержку
+				.finally(() => setTimeout(() => this.setLoading(false), 1000));
+		}
+	},
+	methods: {
+		...mapActions({
+			checkAuthorization: 'authModule/checkAuthorization'
+		}),
+		...mapMutations({
+			deleteToken: 'authModule/deleteToken',
+			deleteUser: 'userModule/deleteUser',
+			setLoading: 'authModule/setLoading'
 		})
 	}
 };
